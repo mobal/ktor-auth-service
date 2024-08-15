1c055e685ee6b5a88ec795ce65b0f409ef1e9883
Balázs Molnár <bmolnar0510@gmail.com>
2024-08-15 11:54:17 +0200

    feat: updated gradle to the latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.jetbrains.kotlin.jvm") version "1.8.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jetbrains.kotlin.jvm") version "2.0.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     application
     idea
     jacoco
@@ -13,39 +13,37 @@ repositories {
 }
 
 dependencies {
-    implementation("com.amazonaws:aws-java-sdk:1.12.384")
-    implementation("io.insert-koin:koin-ktor:3.3.0")
-    implementation("io.insert-koin:koin-logger-slf4j:3.3.0")
-    implementation("io.ktor:ktor-client-content-negotiation:2.2.2")
-    implementation("io.ktor:ktor-client-logging:2.2.2")
-    implementation("io.ktor:ktor-client-okhttp:2.2.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.2")
-    implementation("io.ktor:ktor-server-auth:2.2.2")
-    implementation("io.ktor:ktor-server-auth-jwt:2.2.2")
-    implementation("io.ktor:ktor-server-call-id:2.2.2")
-    implementation("io.ktor:ktor-server-call-logging:2.2.2")
-    implementation("io.ktor:ktor-server-config-yaml:2.2.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.2.2")
-    implementation("io.ktor:ktor-server-core:2.2.2")
-    implementation("io.ktor:ktor-server-netty:2.2.2")
-    implementation("io.ktor:ktor-server-metrics-micrometer:2.2.2")
-    implementation("io.ktor:ktor-server-request-validation:2.2.2")
-    implementation("io.ktor:ktor-server-status-pages:2.2.2")
-    implementation("io.kotless:kotless-lang:0.2.0")
-    implementation("io.kotless:kotless-lang-aws:0.2.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.19.0'")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.72")
-    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
-    implementation("org.glassfish.expressly:expressly:5.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-    implementation("org.springframework.security:spring-security-crypto:6.0.1")
-    testImplementation("io.insert-koin:koin-test:3.3.2")
-    testImplementation("io.ktor:ktor-server-test-host:2.2.2")
+    implementation("com.amazonaws:aws-java-sdk:latest.release")
+    implementation("io.insert-koin:koin-ktor:latest.release")
+    implementation("io.insert-koin:koin-logger-slf4j:latest.release")
+    implementation("io.ktor:ktor-client-content-negotiation:latest.release")
+    implementation("io.ktor:ktor-client-logging:latest.release")
+    implementation("io.ktor:ktor-client-okhttp:latest.release")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:latest.release")
+    implementation("io.ktor:ktor-server-auth:latest.release")
+    implementation("io.ktor:ktor-server-auth-jwt:latest.release")
+    implementation("io.ktor:ktor-server-call-id:latest.release")
+    implementation("io.ktor:ktor-server-call-logging:latest.release")
+    implementation("io.ktor:ktor-server-config-yaml:latest.release")
+    implementation("io.ktor:ktor-server-content-negotiation:latest.release")
+    implementation("io.ktor:ktor-server-core:latest.release")
+    implementation("io.ktor:ktor-server-netty:latest.release")
+    implementation("io.ktor:ktor-server-metrics-micrometer:latest.release")
+    implementation("io.ktor:ktor-server-request-validation:latest.release")
+    implementation("io.ktor:ktor-server-status-pages:latest.release")
+    implementation("org.apache.logging.log4j:log4j-api:latest.release")
+    implementation("org.apache.logging.log4j:log4j-core:latest.release")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:latest.release")
+    implementation("org.bouncycastle:bcprov-jdk18on:latest.release")
+    implementation("org.hibernate.validator:hibernate-validator:latest.release")
+    implementation("org.glassfish.expressly:expressly:latest.release")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:latest.release")
+    implementation("org.springframework.security:spring-security-crypto:latest.release")
+    testImplementation("io.insert-koin:koin-test:latest.release")
+    testImplementation("io.ktor:ktor-server-test-host:latest.release")
     testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
 }
 
 application {
@@ -53,17 +51,13 @@ application {
 }
 
 jacoco {
-    toolVersion = "0.8.8"
+    toolVersion = "0.8.12"
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(22))
     }
-}
-
-ktlint {
-    disabledRules.set(setOf("no-wildcard-imports"))
 }
 
 tasks.jacocoTestReport {
@@ -79,8 +73,8 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     minimize()
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
     }
 }
